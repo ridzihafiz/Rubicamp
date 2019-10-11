c49e9ccb0d743d043078e94d32078622401b621d
Ridzi Hafiz <ridzihafiz@gmail.com>
2019-10-11 14:00:28 +0700

    logic 3

---
+++ C3 Logic 3: Romawi/c3.js
@@ -0,0 +1,12 @@
+function romawi(n) {
+
+}
+
+console.log("Script Testing untuk Konversi Romawi\n");
+console.log("Input | Expected | Result");
+console.log("----- | -------- | ------");
+console.log("4     | IV       |", romawi(4));
+console.log("9     | IX       |", romawi(9));
+console.log("13    | XIII     |", romawi(13));
+console.log("1453  | MCDLIII  |", romawi(1453));
+console.log("1646  | MDCXLVI  |", romawi(1646));
