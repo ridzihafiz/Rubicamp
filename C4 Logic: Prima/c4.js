181c5fef8f0baa23369a94f468f437594fd01445
Ridzi Hafiz <ridzihafiz@gmail.com>
2019-10-14 15:36:09 +0700

    final c4

---
+++ C4 Logic: Prima/c4.js
@@ -0,0 +1,22 @@
+function isPrime(param1) {
+    var nilaiAwal = 0;
+    var number = 1; //setup awal 1 begitu masuk while akan number++
+
+    while (nilaiAwal < param1) {
+        var isPrime = true;
+        number++;
+        for (var i = 2; i <= Math.sqrt(number); i++) { // pengecekan bil prima
+            if (number % i == 0)
+                isPrime = false       
+        }
+        // return param1 > 1
+        if (isPrime) {
+            nilaiAwal++;
+        }
+    }
+    return number;
+}
+
+console.log(isPrime(4))
+console.log(isPrime(500))
+console.log(isPrime(37786))
