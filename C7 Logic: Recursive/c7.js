19e65ed80b8a1078cde1e1e588bf67a41e2ecc6e
Ridzi Hafiz <ridzihafiz@gmail.com>
2019-10-15 16:02:30 +0700

    final c7

---
+++ C7 Logic: Recursive/c7.js
@@ -0,0 +1,17 @@
+function weirdMultiply(num) {
+    var numString = num.toString();
+    // console.log(numString); Testing yang muncul datanya
+    
+    let multiply = 1;
+    if (numString.length === 1) {
+      return numString;
+    } else {
+      for (var i = 0; i < numString.length; i++) {
+        multiply = multiply * numString[i];
+      }
+      return weirdMultiply(multiply)
+      // panggil function dengan hasil perkalian yang terakhir
+      // jika menggunakan parameter num yang diatas(bukan multiply) maka nilai yg pertama lagi yang akan dipanggil
+    }
+  }
+  console.log(weirdMultiply(39));
