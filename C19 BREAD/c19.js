d598213e9c7de680633797270325aea245778f49
Ridzi Hafiz <ridzihafiz@gmail.com>
2019-11-07 22:58:32 +0700

    Completion c19

--- C19 BREAD/c19.js
+++ C19 BREAD/c19.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
-let data = [{id: 1, string: "string 1", integer: 1, float: 1.1, date: 2019/12/12}]
+// let data = [{ id: 1, string: "string 1" }]
 
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('db.json', 'utf8'))
+const write = (data) => fs.writeFileSync('db.json', JSON.stringify(data, null, 3))
 
 const app = express()
 
@@ -17,18 +20,45 @@ app.set('view engine', 'ejs')
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.get('/',(req, res) => res.render('list', {data: data}))
+app.get('/', (req, res) => res.render('list', { data: data }))
 
 app.get('/add', (req, res) => res.render('add'))
 
+app.get('/edit/:id', (req, res) => res.render('edit', {data: data}))
+
 app.post('/add', (req, res) => {
-    data.push({id: req.body.id, string: req.body.string, integer: req.body.integer, float: req.body.float, date: req.body.date})
+    data.push({
+        id: req.body.id,
+        string: req.body.string,
+        integer: req.body.integer,
+        float: req.body.float,
+        date: req.body.date,
+        boolean: req.body.boolean
+    })
+    write(data);
+    // fs.writeFileSync('db.json', JSON.stringify(data, null, 3))
     res.redirect('/')
 })
 
 app.get('/delete/:id', (req, res) => {
     let id = req.params.id
     data.splice(id, 1);
+    write(data);
+    res.redirect('/')
+})
+
+app.post('/edit/:id', (req, res) => {
+    let id = req.params.id
+    // data.splice(id, 1);
+    data[id] = ({
+        id: req.body.id,
+        string: req.body.string,
+        integer: req.body.integer,
+        float: req.body.float,
+        date: req.body.date,
+        boolean: req.body.boolean
+    })
+    write(data);
     res.redirect('/')
 })
 
